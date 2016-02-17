78c7e246f21f45735bf71644ea7eea82db7e74a6
Josh Einstein <josh@josheinstein.com>
2016-02-17 12:32:07 -0500

    Added a Get-EM7DeviceGroupMember function.

--- ScienceLogic/Scripts/DeviceGroups.ps1
+++ ScienceLogic/Scripts/DeviceGroups.ps1
@@ -78,6 +78,97 @@ function Get-EM7DeviceGroup {
 
 ##############################################################################
 #.SYNOPSIS
+# Gets a list of devices that are members of the specified device group.
+##############################################################################
+function Get-EM7DeviceGroupMember {
+
+    [CmdletBinding(DefaultParameterSetName='Advanced')]
+    param(
+
+        # If specified, retrieves the device group with the specified ID
+        [Alias('__DeviceGroupID')]
+        [Parameter(ParameterSetName='ID', Position=1, Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+        [Int32[]]$ID,
+
+        # If specifieed, the keys of this hashtable are prefixed with
+        # 'filter.' and used as filters. For example: @{state='PA'}
+        [Parameter(ParameterSetName='Advanced')]
+        [Hashtable]$Filter,
+
+        # If specified, device groups are searched based on the name
+        # Wildcards can be used at either end of the name to check for
+        # partial matches.
+        [Parameter(ParameterSetName='Advanced')]
+        [String]$Name,
+
+        # Limits the results to the specified number. The default is 1000.
+        [Parameter()]
+        [Int32]$Limit = $Globals.DefaultLimit,
+
+        # The starting offset in the results to return.
+        # If retrieving objects in pages of 100, you would specify 0 for page 1,
+        # 100 for page 2, 200 for page 3, and so on.
+        [Parameter()]
+        [Int32]$Offset = 0,
+
+        # Optionally sorts the results by this field in ascending order, or if
+        # the field is prefixed with a dash (-) in descending order.
+        # You can also pipe the output to PowerShell's Sort-Object cmdlet, but
+        # this parameter is processed on the server, which will affect how
+        # results are paginated when there are more results than fit in a
+        # single page.
+        [Parameter()]
+        [String]$OrderBy,
+
+        # Specifies one or more property names that ordinarily contain a link
+        # to a related object to automatically retrieve and place in the 
+        # returned object.
+        [Parameter()]
+        [String[]]$ExpandProperty,
+
+        # If specified, device groups that are members of this device group will
+        # be recursively expanded as well.
+        [Parameter()]
+        [Switch]$Recurse
+
+    )
+
+    begin {
+    
+        EnsureConnected -ErrorAction Stop
+
+    }
+
+    process {
+
+        $DeviceGroups = @()
+        if ($ID)   { $DeviceGroups += @(Get-EM7DeviceGroup -ID:$ID) }
+        if ($Name) { $DeviceGroups += @(Get-EM7DeviceGroup -Name:$Name) }
+
+        foreach ($DeviceGroup in $DeviceGroups) {
+
+            $URI = CreateUri "device_group/$($DeviceGroup.__ID)/expanded_devices"
+            $Response = HttpInvoke $URI
+            
+            $GroupIDs = [Int32[]]($DeviceGroup.groups -replace '.*/(\d+)$','$1')
+            $MemberIDs = [Int32[]]($Response -replace '.*/(\d+)$','$1')
+
+            if ($MemberIDs) {
+                Get-EM7Object device -ID:$MemberIDs -ExpandProperty:$ExpandProperty
+            }
+
+            if ($Recurse -and $GroupIDs) {
+                Get-EM7DeviceGroupMember -ID:$GroupIDs -ExpandProperty:$ExpandProperty
+            }
+
+        }
+
+    }
+
+}
+
+##############################################################################
+#.SYNOPSIS
 # Adds a device as a static member of the specified device group.
 ##############################################################################
 function Add-EM7DeviceGroupMember {
